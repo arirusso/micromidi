9513304631b1feccc47176da1519a0187a56630c
Ari Russo <ari.russo+github.com@gmail.com>
2011-08-16 16:28:16 -0300

    Edited examples/midi_message_objects.rb via GitHub

--- examples/midi_message_objects.rb
+++ examples/midi_message_objects.rb
@@ -3,10 +3,24 @@ $:.unshift File.join( File.dirname( __FILE__ ), '../lib')
 
 require "micromidi"
 
-output = UniMIDI::Output.first.open
-
 MIDI.message do
+  
+  # set sticky values
   channel 4
   velocity 120
-  play "C4", 1
+  
+  # create some objects
+  note_msg = note "C4"
+  controller_msg = cc 2, 120
+  patch_change_msg = pc 5
+  
+  # inspect the messages
+  p note_msg
+  p controller_msg
+  p patch_change_msg
+  
 end 
+
+#
+# this should print something like the following to the console:
+#
