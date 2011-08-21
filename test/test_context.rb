ab3895c8d2baa708d0fbf8c6bef50ae34922b711
Ari Russo <ari.russo@gmail.com>
2011-08-21 14:22:21 -0400

    context test

---
+++ test/test_context.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+
+require 'helper'
+
+class ContextTest < Test::Unit::TestCase
+
+  include MicroMIDI
+  include MIDIMessage
+  include TestHelper
+
+  def test_repeat
+    m = MicroMIDI.message
+    msg = m.note "C0"
+    assert_equal(NoteOn, msg.class)
+    assert_equal(12, msg.note)
+    assert_equal(0, msg.channel)
+    
+    r_msg = m.repeat
+    assert_equal(NoteOn, r_msg.class)
+    assert_equal(12, r_msg.note)
+    assert_equal(0, r_msg.channel)
+  end
+          
+end
+
