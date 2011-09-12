276bbe8c6bfd9abb7bc81896614f8f4ed1002f74
Ari Russo <ari.russo@gmail.com>
2011-09-11 22:04:13 -0400

    allow node to be set on generic sysex message

--- lib/micromidi/instructions/sysex.rb
+++ lib/micromidi/instructions/sysex.rb
@@ -30,7 +30,9 @@ module MicroMIDI
       
       # create an indeterminate sysex message
       def sysex_message(data, options = {})
-        SystemExclusive::Message.new(data, options)
+        options[:sysex_node] ||= options[:node]
+        props = @state.message_properties(options, :sysex_node)
+        SystemExclusive::Message.new(data, :node => props[:sysex_node])
       end
       alias_method :sysex, :sysex_message
 
