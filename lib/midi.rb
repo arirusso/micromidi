d9d4aed6415e4e770ee12414ce92edde379a8a32
Ari Russo <ari.russo@gmail.com>
2011-09-17 19:32:55 -0400

    allow require midi or require micromidi

---
+++ lib/midi.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+#
+# micromidi
+# A Ruby DSL for MIDI
+#
+# (c)2011 Ari Russo 
+# licensed under the Apache 2.0 License
+#
+
+require 'micromidi'
--- test/helper.rb
+++ test/helper.rb
@@ -4,7 +4,7 @@ dir = File.dirname(File.expand_path(__FILE__))
 $LOAD_PATH.unshift dir + '/../lib'
 
 require 'test/unit'
-require 'micromidi'
+require 'midi'
 
 module TestHelper
   
