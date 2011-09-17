8b436b98ca0d5ee9f336ff25fb52460d7f4e5eb5
Ari Russo <ari.russo@gmail.com>
2011-09-17 19:34:31 -0400

    allow require midi or require micromidi

--- lib/midi.rb
+++ lib/midi.rb
@@ -7,4 +7,9 @@
 # licensed under the Apache 2.0 License
 #
 
+# the purpose of this file is just to allow both 
+# <em>require "micromidi"</em>
+# and 
+# <em>require "midi"</em>
+
 require 'micromidi'
