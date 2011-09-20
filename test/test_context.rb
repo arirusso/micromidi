47649cb1b92a4812972d3e11b5e962708d1ee47c
Ari Russo <ari.russo@gmail.com>
2011-09-20 00:11:34 -0400

    test edit and new with various block passing scenarios

--- test/test_context.rb
+++ test/test_context.rb
@@ -8,12 +8,33 @@ class ContextTest < Test::Unit::TestCase
   include MIDIMessage
   include TestHelper
   
-  def test_no_block
+  def test_new_with_block
+    msg = nil
+    MIDI::IO.new do
+      msg = note "C0"
+    end
+    assert_equal(NoteOn, msg.class)
+    assert_equal(12, msg.note)
+    assert_equal(0, msg.channel)    
+  end
+  
+  def test_new_with_no_block
     m = MIDI::IO.new
     msg = m.note "C0"
     assert_equal(NoteOn, msg.class)
     assert_equal(12, msg.note)
     assert_equal(0, msg.channel)
+  end
+  
+  def test_edit
+    msg = nil
+    m = MIDI::IO.new 
+    m.edit do
+      msg = m.note "C0"
+    end
+    assert_equal(NoteOn, msg.class)
+    assert_equal(12, msg.note)
+    assert_equal(0, msg.channel)     
   end
 
   def test_repeat
