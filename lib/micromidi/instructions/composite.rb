f5df49e90a8a136788c153d137fd39e40e74a742
Ari Russo <ari.russo@gmail.com>
2011-11-07 20:32:36 -0500

    play chord with flexible args

--- lib/micromidi/instructions/composite.rb
+++ lib/micromidi/instructions/composite.rb
@@ -6,15 +6,32 @@ module MicroMIDI
     
     module Composite
       
-      # play note n, wait <em>duration</em> seconds, then do note off for note n
-      def play(n, duration)
-        msg = case n
-          when Numeric, String then note(n)
-          when MIDIMessage then n 
+      #
+      # plays a note or notes, for a certain duration.
+      #
+      # the first argument must be a note name (String), MIDIMessage::NoteOn object, or array of either
+      # the last argument must be a Numeric (representing the duration)
+      #
+      # additional arguments should be note names or MIDIMessage::NoteOn objects and will 
+      # be played as a chord with the first argument
+      #
+      def play(*args)
+        raise "last argument must be a Numeric duration" unless args.last.kind_of?(Numeric)
+        
+        duration = args.pop
+        note_or_notes = [args].flatten
+        
+        msgs = note_or_notes.map do |n|
+          case n
+            when Numeric, String then note(n)
+            when MIDIMessage then n 
+          end
         end
+        
         sleep(duration)
-        off
-        msg
+        msgs.each { |msg| note_off(msg.note, :channel => msg.channel, :velocity => msg.velocity) }
+        
+        msgs.size > 1 ? msgs : msgs.first
       end
       
       # sends a note off message for every note on every channel
--- test/test_composite.rb
+++ test/test_composite.rb
@@ -28,6 +28,50 @@ class CompositeTest < Test::Unit::TestCase
     assert_equal(0, off_msg.channel)
   end
   
+  def test_play_chord
+    m = MicroMIDI.message
+    start = Time.now
+    msgs = m.play "C0", "E1", "G2", 0.5
+    
+    finish = Time.now
+    dif = finish - start
+    assert_equal(true, dif >= 0.5)
+    
+    msg = msgs.first
+    
+    assert_equal(NoteOn, msg.class)
+    assert_equal(12, msg.note)
+    assert_equal(0, msg.channel)
+    assert_equal(6, m.state.output_cache.size)
+    
+    off_msg = m.state.output_cache.last[:message]
+    assert_equal(NoteOff, off_msg.class)
+    assert_equal(43, off_msg.note)
+    assert_equal(0, off_msg.channel)
+  end
+  
+  def test_play_chord_array
+    m = MicroMIDI.message
+    start = Time.now
+    msgs = m.play ["C0", "E1", "G2"], 0.5
+    
+    finish = Time.now
+    dif = finish - start
+    assert_equal(true, dif >= 0.5)
+    
+    msg = msgs.first
+    
+    assert_equal(NoteOn, msg.class)
+    assert_equal(12, msg.note)
+    assert_equal(0, msg.channel)
+    assert_equal(6, m.state.output_cache.size)
+    
+    off_msg = m.state.output_cache.last[:message]
+    assert_equal(NoteOff, off_msg.class)
+    assert_equal(43, off_msg.note)
+    assert_equal(0, off_msg.channel)
+  end
+  
   def test_all_off
     
   end
