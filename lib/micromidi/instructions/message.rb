0370b2c6d76fa0737a77533aca54120d2609b36e
Ari Russo <ari.russo@gmail.com>
2014-09-07 18:41:16 -0400

    unpack

--- lib/micromidi/instructions/message.rb
+++ lib/micromidi/instructions/message.rb
@@ -3,23 +3,25 @@ module MicroMIDI
   module Instructions
     
     class Message
-
-      include MIDIMessage
       
       def initialize(state)
         @state = state
       end
       
       # create a control change message
-      def control_change(id, value, opts = {})
-        props = @state.message_properties(opts, :channel)
-        id.kind_of?(Numeric) ? ControlChange.new(props[:channel], id, value) : ControlChange[id].new(props[:channel], value)
+      def control_change(id, value, options = {})
+        properties = @state.message_properties(options, :channel)
+        if id.kind_of?(Numeric)
+          MIDIMessage::ControlChange.new(properties[:channel], id, value)
+        else
+          MIDIMessage::ControlChange[id].new(properties[:channel], value)
+        end
       end
 
       # create a note message
       def note(id, options = {})
         properties = @state.message_properties(options, :channel, :velocity)
-        note = note_message(NoteOn, id, properties)
+        note = note_message(MIDIMessage::NoteOn, id, properties)
         @state.last_note = note
         note
       end
@@ -27,7 +29,7 @@ module MicroMIDI
       # create a note off message
       def note_off(id, options = {})
         properties = @state.message_properties(options, :channel, :velocity)
-        note_message(NoteOff, id, properties)
+        note_message(MIDIMessage::NoteOff, id, properties)
       end
 
       # create a MIDI message from a byte string, array of bytes, or list of bytes
@@ -36,37 +38,37 @@ module MicroMIDI
       end
 
       # create a program change message
-      def program_change(program, opts = {})
-        props = @state.message_properties(opts, :channel)
-        MIDIMessage::ProgramChange.new(props[:channel], program)
+      def program_change(program, options = {})
+        properties = @state.message_properties(options, :channel)
+        MIDIMessage::ProgramChange.new(properties[:channel], program)
       end
 
       # create a note-off message from the last note-on message
       def off
-        o = @state.last_note.to_note_off unless @state.last_note.nil?
+        note_off = @state.last_note.to_note_off unless @state.last_note.nil?
         @state.last_note = nil
-        o
+        note_off
       end
       
       # create a channel pressure message
-      def channel_aftertouch(value, opts = {})
-        props = @state.message_properties(opts, :channel)
-        MIDIMessage::ChannelAftertouch.new(props[:channel], value)
+      def channel_aftertouch(value, options = {})
+        properties = @state.message_properties(options, :channel)
+        MIDIMessage::ChannelAftertouch.new(properties[:channel], value)
       end
       alias_method :channel_pressure, :channel_aftertouch
       
       # create a poly pressure message
-      def polyphonic_aftertouch(note, value, opts = {})
-        props = @state.message_properties(opts, :channel)
-        MIDIMessage::PolyphonicAftertouch.new(props[:channel], note, value)
+      def polyphonic_aftertouch(note, value, options = {})
+        properties = @state.message_properties(options, :channel)
+        MIDIMessage::PolyphonicAftertouch.new(properties[:channel], note, value)
       end
       alias_method :poly_aftertouch, :polyphonic_aftertouch
       alias_method :polyphonic_pressure, :polyphonic_aftertouch
       alias_method :poly_pressure, :polyphonic_aftertouch
       
-      def pitch_bend(low, high, opts = {})
-        props = @state.message_properties(opts, :channel)
-        MIDIMessage::PitchBend.new(props[:channel], low, high)
+      def pitch_bend(low, high, options = {})
+        properties = @state.message_properties(options, :channel)
+        MIDIMessage::PitchBend.new(properties[:channel], low, high)
       end
       alias_method :bend, :pitch_bend
       alias_method :pitchbend, :pitch_bend
