e0ca548695444fa76d79b85ed4938dd62571cd62
Ari Russo <ari.russo@gmail.com>
2014-10-26 18:31:05 -0400

    don't include module

--- lib/micromidi/instructions/sysex.rb
+++ lib/micromidi/instructions/sysex.rb
@@ -1,36 +1,34 @@
 module MicroMIDI
 
   module Instructions
-    
+
     class SysEx
 
-      include MIDIMessage
-      
       def initialize(state)
         @state = state
       end
-      
+
       # create a sysex command
       def sysex_command(address, data, options = {})
         options[:sysex_node] ||= options[:node]
         props = @state.message_properties(options, :sysex_node)
-        SystemExclusive::Command.new(address, data, :node => props[:sysex_node])
+        MIDIMessage::SystemExclusive::Command.new(address, data, :node => props[:sysex_node])
       end
       alias_method :command, :sysex_command
-      
+
       # create a sysex request
       def sysex_request(address, size, options = {})
         options[:sysex_node] ||= options[:node]
         props = @state.message_properties(options, :sysex_node)
-        SystemExclusive::Request.new(address, size, :node => props[:sysex_node])        
+        MIDIMessage::SystemExclusive::Request.new(address, size, :node => props[:sysex_node])
       end
       alias_method :request, :sysex_request
-      
+
       # create an indeterminate sysex message
       def sysex_message(data, options = {})
         options[:sysex_node] ||= options[:node]
         props = @state.message_properties(options, :sysex_node)
-        SystemExclusive::Message.new(data, :node => props[:sysex_node])
+        MIDIMessage::SystemExclusive::Message.new(data, :node => props[:sysex_node])
       end
       alias_method :sysex, :sysex_message
 
