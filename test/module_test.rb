647a2843447444604a7e38f0f266f6279219949b
Ari Russo <ari.russo@gmail.com>
2015-04-10 21:00:34 -0400

    basic test for MIDI module

--- test/module_test.rb
+++ test/module_test.rb
@@ -4,6 +4,11 @@ class ModuleTest < Minitest::Test
 
   context "MIDI" do
 
+    should "have MIDI module" do
+      refute_nil MIDI
+      assert_equal MicroMIDI, MIDI
+    end
+
     should "add UniMIDI modules" do
       assert_equal UniMIDI::Input, MIDI::Input
       assert_equal UniMIDI::Output, MIDI::Output
